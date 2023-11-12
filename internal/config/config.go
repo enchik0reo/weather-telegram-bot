d3f00cc19cf60aee6737d89dcd4683598f4044ae
enchik0reo <profipingvin@gmail.com>
2023-11-12 20:29:50 +0700

    Change config

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,7 +13,6 @@ import (
 
 type Config struct {
 	Token string `env:"TOKEN_TG_BOT"`
-	Port  string `yaml:"port" env-default:"8080"`
 	DB    struct {
 		Host     string `yaml:"host"`
 		Port     string `yaml:"port"`
@@ -24,44 +23,47 @@ type Config struct {
 	} `yaml:"db"`
 }
 
-func Load() (c *Config, err error) {
-	c, err = readYml(c)
+func Load() (*Config, error) {
+	var c = Config{}
+	var err error
+
+	err = readYml(&c)
 	if err != nil {
 		return nil, err
 	}
 
-	c, err = loadEnv(c)
+	err = loadEnv(&c)
 	if err != nil {
 		return nil, err
 	}
 
-	return
+	return &c, nil
 }
 
-func readYml(c *Config) (*Config, error) {
+func readYml(c *Config) error {
 	p := filepath.Join("config", "cfg.yml")
 	if err := cleanenv.ReadConfig(p, c); err != nil {
 		_, err = cleanenv.GetDescription(c, nil)
-		return nil, e.Wrap("an error occurred while getting config", err)
+		return e.Wrap("an error occurred while getting config", err)
 	}
 
-	return c, nil
+	return nil
 }
 
-func loadEnv(c *Config) (*Config, error) {
+func loadEnv(c *Config) error {
 	if err := godotenv.Load(); err != nil {
-		return nil, e.Wrap("an error occurred while loading environment variables", err)
+		return e.Wrap("an error occurred while loading environment variables", err)
 	}
 
 	c.Token = os.Getenv("TOKEN_TG_BOT")
 	if c.Token == "" {
-		return nil, e.Wrap("token is not specified in environment variables", fmt.Errorf("just write it down"))
+		return e.Wrap("token is not specified in environment variables", fmt.Errorf("just write it down"))
 	}
 
 	c.DB.Password = os.Getenv("POSTGRES_PASSWORD")
 	if c.DB.Password == "" {
-		return nil, e.Wrap("db password is not specified in environment variables", fmt.Errorf("just write it down"))
+		return e.Wrap("db password is not specified in environment variables", fmt.Errorf("just write it down"))
 	}
 
-	return c, nil
+	return nil
 }
